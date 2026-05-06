cf636518dd0583960ac42a74f91a26693f6c3e65
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-05-06 14:48:44 -0500

    Uso de valores decimales en el ejercicio de punteros

--- Ejercicio.c
+++ Ejercicio.c
@@ -4,6 +4,11 @@ int main()
     int num=150, a;
     int *x;//puntero
     x= &num;//Almacena la direcion de num
-    printf("%d %p",num, x);
+    printf("\n%d %p",num, x);
+    float pi=3.1415, decimal;
+    float *ptrf;
+    ptrf=&pi;
+
+    printf("\n%f %p",pi, ptrf);
     return 0;
 }
