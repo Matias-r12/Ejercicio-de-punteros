765b149d02eb4651de39109286cf2dd1ff4816ba
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-05-06 15:08:48 -0500

    Añadimos otra variante de puntero con caracteres

--- Ejercicio.c
+++ Ejercicio.c
@@ -4,13 +4,19 @@ int main()
     int num=150, a;
     int *x;//puntero
     x= &num;//Almacena la direcion de num
-    a=*x; //contenido de la dirrecion de memoria
+    a=*x; //contenido de la dirrecion de memoria en x
     printf("\n%d %d %p",num,a, x);
     float pi=3.1415, decimal;
     float *ptrf;//puntero
     ptrf=&pi;//Almacena la direcion de ptrf
     decimal=*ptrf;
     printf("\n%f %f %p",pi,decimal, ptrf);
+    char letra='a',caracter;
+    char *ptrc; //definicion del puntero
+    ptrc=&letra;//puntero que almacena la dirreccion de letra
+    caracter=*ptrc;//contenido de la dirrecion de memoria en ptrc
+    printf("\n%c %c %p",letra,caracter,ptrc);
+
 
     return 0;
 }
