80b8e3fe88c67a8284a93d6b22de14418fc729f3
Matias.Rojas <matias.rojas.suntaxi@udla.edu.ec>
2026-05-06 15:01:46 -0500

    Añadimos otro almacenamiento de contenido de la dirrecion de memoria tanto entero y decimal

--- Ejercicio.c
+++ Ejercicio.c
@@ -4,11 +4,13 @@ int main()
     int num=150, a;
     int *x;//puntero
     x= &num;//Almacena la direcion de num
-    printf("\n%d %p",num, x);
+    a=*x; //contenido de la dirrecion de memoria
+    printf("\n%d %d %p",num,a, x);
     float pi=3.1415, decimal;
-    float *ptrf;
-    ptrf=&pi;
+    float *ptrf;//puntero
+    ptrf=&pi;//Almacena la direcion de ptrf
+    decimal=*ptrf;
+    printf("\n%f %f %p",pi,decimal, ptrf);
 
-    printf("\n%f %p",pi, ptrf);
     return 0;
 }
